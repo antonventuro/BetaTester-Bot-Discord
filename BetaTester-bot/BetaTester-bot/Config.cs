9fafb8c4c3160842d27a38a6cb86bb11bc4b5e90
Hector Costa <hcostaguzman@gmail.com>
2017-03-14 02:04:04 +0100

    He cambiado el token y otro fichero, comiteo a ver que ocurre.

--- BetaTester-bot/BetaTester-bot/Config.cs
+++ BetaTester-bot/BetaTester-bot/Config.cs
@@ -8,6 +8,6 @@ namespace BetaTester_bot
 {
     public static class Config
     {
-        public const string Token = "TOKEN_HERE";
+        public const string Token = "MjkwODk5Nzc2Njg0NDI1MjE3.C6hpHw.NbAbt2v0m24HU79tFMffm0fmpCs";
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BetaTester-Bot-Discord
+
+Para utilizar el Bot hay que configurar el token en Config.cs, estos cambios son ignorados por Git.
+
+```csharp
+public const string Token = "YOUR_TOKEN";
+```
